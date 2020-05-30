3e9c54cefc0461d16d4b5fb07307c2b7bf0bc933
Daniel Paiva <danpaiva@live.com>
2020-05-30 17:05:04 -0300

    Add Form Covid
    Former-commit-id: 32014ea3dda1b3d816834af45f120d3b8cff8031
    Former-commit-id: 56923533be61913283f0dc921dcfef26589bd8a0
    Former-commit-id: a8e4b07c2e226166a05a843d255293718303a0ac
    Former-commit-id: 7c6dd1bd40f92fef0fab57a335c622c9e59f38c0

---
+++ UPA HELPER/UPE HELPER/Formularios/Covid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UPA_HELPER.Formularios
+{
+    public partial class Covid : Form
+    {
+        public Covid()
+        {
+            InitializeComponent();
+        }
+    }
+}
