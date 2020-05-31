413d53dceff94fb4b2ca1edf0c631181ab788eb7
Daniel Paiva <danpaiva@live.com>
2020-05-31 00:16:15 -0300

    Add BackGround image - Upa Helper User | Some Fixes
    Former-commit-id: 51595b47bb0af9ae828c4921202db706743ee6e2
    Former-commit-id: 66c91418a4cefac01d661b0a5372150e72e0498c
    Former-commit-id: 7103961e2266f13d3ea601a3739672bc7435fe24
    Former-commit-id: b39563058c8fad080f92dccd8f963a4bc4718dfe

---
+++ UPA Helper User/UpaHelperUser/UpaHelperUser/Telefone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpaHelperUser
+{
+    public partial class Telefone : Form
+    {
+        public Telefone()
+        {
+            InitializeComponent();
+        }
+    }
+}
