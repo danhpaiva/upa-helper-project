d248c3d2ce2b726cd9ca4889dfb7fe5c3dfa4efc
Daniel Paiva <danpaiva@live.com>
2020-06-19 14:15:55 -0300

    Revisão de código
    Former-commit-id: 32f1e7cb6e1b19c19bb5767e6142565d278dafb2
    Former-commit-id: e9e44da5e825133369378ffa34b39800ff91f7b9
    Former-commit-id: 78aa5b2bff9ec3962cbd2556b469e4d20aaba6b6
    Former-commit-id: 66a749d8eceb972f353dd1338daab7d303d3b5cc

--- UPA Helper User/UpaHelperUser/UpaHelperUser/Formulario/DEV.cs
+++ UPA Helper User/UpaHelperUser/UpaHelperUser/Formulario/DEV.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UpaHelperUser
@@ -20,6 +13,11 @@ namespace UpaHelperUser
         private void btn_sair_Click(object sender, EventArgs e)
         {
             Close(); //Fechar a janela atual
+        }
+
+        private void DEV_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
