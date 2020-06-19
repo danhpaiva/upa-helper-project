fc412f8276186c7115a18139aacd7b51f223ed47
Daniel Paiva <danpaiva@live.com>
2020-06-19 13:44:05 -0300

    Revisão de código
    Former-commit-id: 96f730be2ecc9bc04ff9bce9e0347e9c053cb838
    Former-commit-id: ca9e6aa6b20fbe74629bee2d3ac66f2f1048532f
    Former-commit-id: 85f294b516f60037b91a0cbe13df262c36a705e3
    Former-commit-id: 13b38841791147e5e8a68da189f7a3abc5c64127

--- UPA HELPER/UPE HELPER/Formularios/TelefoneUtil.cs
+++ UPA HELPER/UPE HELPER/Formularios/TelefoneUtil.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UPA_HELPER
@@ -20,6 +13,11 @@ namespace UPA_HELPER
         private void btn_sair_Click(object sender, EventArgs e)
         {
             Close(); //Fechando tela atual
+        }
+
+        private void TelefoneUtil_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
